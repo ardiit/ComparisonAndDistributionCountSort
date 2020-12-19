435e32610e1ef3977847a92b9b9ae863457bb4d2
DC <DC@DESKTOP-69F643K>
2020-12-19 10:52:49 +0100

    Add project files.

---
+++ ComparisonAndDistributionCountSort/Comparison.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComparisonAndDistributionCountSort
+{
+    public static class Comparison
+    {
+        // Low to High sort
+        public static void BubbleSort(int[] array, int arrayLength)
+        {
+            int i, j, temp;
+            bool swapped;
+
+            for (i = 0; i < arrayLength - 1; i++)
+            {
+                swapped = false;
+                for (j = 0; j < arrayLength - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        // SWAPP
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (swapped == false)
+                    break;
+            }
+        }
+
+    }
+}
---
+++ ComparisonAndDistributionCountSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ComparisonAndDistributionCountSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] randomArray = Enumerable.Range(1, 1000000).OrderBy(b => random.Next()).ToArray();
+
+            Stopwatch stopwatch1 = new Stopwatch();
+
+            stopwatch1.Start();
+            Comparison.BubbleSort(randomArray, randomArray.Length);
+            stopwatch1.Stop();
+
+
+            Stopwatch stopwatch2 = new Stopwatch();
+
+            stopwatch2.Start();
+            Distribution.CountingSort(randomArray, randomArray.Length);
+            stopwatch2.Stop();
+
+            Console.WriteLine($"Comparison Algorithm time:{stopwatch1.Elapsed}");
+            Console.WriteLine($"Distribution Algorithm time:{stopwatch2.Elapsed}");
+
+        }
+    }
+}
